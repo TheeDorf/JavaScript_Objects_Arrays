955fa08bf1b24dc54fb102b76ca7102bde451ac5
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-16 12:01:00 -0700

    exercise 1 and 2

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const arrSum = [2,22,12,17,18,39,129];
+
+let sum=0;
+for (let i=0; i < arrSum.length; i++){
+    sum += arrSum[i];
+   
+}
+console.log(sum);
+
+
+
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+let book = {};
 
+book.title= "The Outsiders"
+book.author = "S.E. Hinton"
+book.pages = 192
+book.readCount = 2
+
+book.info = function(){
+    return `${book.title} by ${book.author}, ${book.pages} pages, read ${book.readCount} times.`
+};
+
+console.log(book.info());
