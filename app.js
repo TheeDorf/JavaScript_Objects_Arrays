f4052f1bbc91d0a1ade2457e683f5ebac47a40a4
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-17 11:55:51 -0700

    finish exercises

--- app.js
+++ app.js
@@ -42,17 +42,65 @@ book.info()
 
 // //  ex 3
 
-let sentence = "The quick brown fox jumps over the lazy dog.";
+let sentence = "The quick brown fox jumps over the lazy dog";
 let arr1 = sentence.split(" ");
 
 for (let i = 0; i < arr1.length; i++) {
-    let sentence = arr1[i];
+    let letters = arr1[i].split("");
+    letters.reverse();
+    arr1[i] = letters.join("");
     
 }
-
-console.log(arr1)
-
-
+let lettersReverse = arr1.join(" ");
+console.log(lettersReverse)
 
 
+//  OTHER WAY TO DO ABOVE
+// let sentence = "The quick brown fox jumps over the lazy dog";
+// function reverseWord (sentence) {
+//     let result = [];
+//     let words = sentence.split(" ");
+//     console.log("These are the words split", words);
+//     for (let i = 0; i < words.length; i++) {
+//         let letters = words[i].split("");
+//         console.log("Here are the letters split", letters);
+//         for (let j = letters.length - 1; j >= 0; j--) {
+//             result.push(letters[j]);
+//         }
+//     }
+//     console.log("This is after the push for result", result)
+//     return result
+// }
 
+// console.log(reverseWord(sentence));
+
+//  ex4
+// let cup = {
+//     shape:"round",
+//     content: "empty",
+//     height: 25
+// }
+
+
+// ex 4 
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+const head1 = csvData.split("\n")[0].split(",")[0]
+const head2 = csvData.split("\n")[0].split(",")[1]
+
+const data = csvData.split("\n").slice(1)
+myArr = []
+const myObj = {
+    [head1]: "",
+    [head2]: ""
+}
+
+data.forEach((val) => {
+    let dataVal =val.split(",")
+    console.log(dataVal)
+    let dataObj = Object.create(myObj);
+    dataObj[head1]= dataVal[0];
+    dataObj[head2]= dataVal[1];
+    myArr.push(dataObj)
+    
+});
+console.log(myArr);
